c2e55f22d2f55c10cd1612faf1fa89b4d63df838
cj <cj@localhost.localdomain>
2016-11-16 20:35:48 +0800

    编写登录的回调函数

---
+++ dist/assets/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#login').click(function() {
+		var userName = $('#username').val();
+		var password = $('#password').val();
+
+		/**
+		 * 此处加入登录代码
+		 */
+		console.log(userName, password);
+	});
+});
